eb0e1b6553aeb13cfab7d9676dee053c64e205b9
jawwadmemon74 <jawwadmemon74@hotmail.com>
2017-11-12 15:31:47 +0500

    lastcommit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import MuiThemeProvider from '/node_modules/material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from './material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(<BrowserRouter><MuiThemeProvider><App /></MuiThemeProvider></BrowserRouter>, document.getElementById('root'));
